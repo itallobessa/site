f283fe2e75ab02fc43881da2ac91054b3b0e01a7
Italo Bessa <italosupera@hotmail.com>
2022-07-11 21:25:54 -0300

    Pequenas alterações
    Pequenas alterações final de aula, menu

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-
-
-var hamburguer = document.querySelector(".hamburguer");
-
-hamburguer.addEventListener("click", function(){
+document.querySelector(".hamburguer").addEventListener("click", () => 
     document.querySelector(".container").classList.toggle("show-menu")
-});
+);
